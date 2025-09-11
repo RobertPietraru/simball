2c8030671e6711087cda584d4c9baaa27ae349aa
RobertPietraru <rob_piet@yahoo.com>
2025-09-11 16:56:28 +0300

    fix seeding

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,6 @@
 import log from '$lib/logging.js';
 export const load = async ({ locals, url, cookies }) => {
     const user = locals.user;
-    console.log('cookies', cookies.getAll());
     const isAuthPage = url.pathname.includes('auth');
     return {
         user,
